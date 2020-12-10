f3d43fcb68bf022773df86b6660f349b84211577
Pavel Zaletski <pavel_zaletski@epam.com>
2020-12-10 10:51:22 +0300

    feat: Add https to cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://iewur1phgg.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://nwngvq0ia6.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://pavelzaletski-cart-api-dev.eu-west-1.elasticbeanstalk.com',
+  cart: 'https://vvoy30i85k.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
